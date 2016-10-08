60c427aef8e69bf720f24810e2849f1fb7c3040a
Brian Wright <brianwright@mail.usf.edu>
2016-10-08 18:21:46 -0400

    Initial commit

---
+++ include/TowerDefense.h
@@ -0,0 +1,6 @@
+#ifndef TOWER_DEFENSE_TOWERDEFENSE_H_
+#define TOWER_DEFENSE_TOWERDEFENSE_H_
+
+int CreateTower(void);
+
+#endif
---
+++ src/TowerDefense.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "TowerDefense.h"
+
+
+int main(int argc, char *argv[]) {
+  std::cout << "TEST" << std::endl;
+  return 0;
+}
